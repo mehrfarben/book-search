cb4c06990bd3322c95c713f21e3462065bfc4786
Alim Karaca <alim.karaca2000@gmail.com>
2024-02-23 10:22:10 +0300

    Added placeholder image and enter search

--- script.js
+++ script.js
@@ -1,33 +1,42 @@
+const searchInput = document.getElementById("searchInput");
+
+searchInput.addEventListener("keyup", (event) => {
+  if (event.key === "Enter") {
+    document.getElementById("searchBtn").click();
+  }
+});
+
 function searchBooks() {
-    const searchInput = document.getElementById('searchInput').value;
-    const url = 'https://www.googleapis.com/books/v1/volumes?q=$'+ searchInput +'&maxResults=30';
-   
+  const url = "https://www.googleapis.com/books/v1/volumes?q=$" + searchInput.value + "&maxResults=30";
+  const resultsContainer = document.getElementById("results");
 
+  if (!searchInput) {
+    resultsContainer.innerHTML = "Kitap Adı Giriniz.";
+  } else {
     fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            const resultsContainer = document.getElementById('results');
-            resultsContainer.innerHTML = '';
+      .then((response) => response.json())
+      .then((data) => {
+        resultsContainer.innerHTML = "";
 
-            if (data.items && data.items.length > 0) {
-                data.items.forEach(book => {
-                    const title = book.volumeInfo.title;
-                    const authors = book.volumeInfo.authors ? book.volumeInfo.authors.join(', ') : 'Bilinmeyen Yazar';
-                    const thumbnail = book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : '';
+        if (data.items && data.items.length > 0) {
+          data.items.forEach((book) => {
+            const title = book.volumeInfo.title;
+            const authors = book.volumeInfo.authors ? book.volumeInfo.authors.join(", ") : "Bilinmeyen Yazar";
+            const thumbnail = book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : "";
 
-                    resultsContainer.innerHTML += `
+            resultsContainer.innerHTML += `
                         <div>
                             <h3>${title}</h3>
                             <p>Yazar: ${authors}</p>
-                            <img src="${thumbnail}" alt="${title}">
+                            <img src="${thumbnail}" alt="${title}" onerror="this.src='https://placehold.co/120x170';">
                             <hr color=black>
                         </div>
                     `;
-                });
-            } else {
-                resultsContainer.innerHTML = 'Kitap Bulunamadı.';
-            }
-      
-        })
-        .catch(error => console.error('Veri getirilemedi:', error));
+          });
+        } else {
+          resultsContainer.innerHTML = "Kitap Bulunamadı.";
+        }
+      })
+      .catch((error) => console.error("Veri getirilemedi:", error));
+  }
 }
